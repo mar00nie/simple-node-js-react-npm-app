0093a656f4677f4c48c61bd32615ea3c405166ea
Ynes <yangyangteng@gmail.com>
2019-07-30 22:08:52 +0100

    new

--- pipeline.sh
+++ pipeline.sh
@@ -1,12 +1,11 @@
 # Set pipeline
-fly -t node-app sp -c simple-node-js-react-npm-app/job-node.yml -p build-test-deploy
+fly -t node-app sp -c simple-node-js-react-npm-app/job-node.yml -p build-pipeline
 
 # Unpause pipeline
-fly -t node-app up -p build-test-deploy
+fly -t node-app up -p build-pipeline
 
 # Trigger pipeline jobs
-
-fly -t node-app tj -j build-test-deploy/run-pipeline -w
+fly -t node-app tj -j build-pipeline/run-pipeline -w
 
 # List of builds
 fly -t node-app builds
